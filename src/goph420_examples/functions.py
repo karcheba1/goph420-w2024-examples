4e330c987bcb922515f0c3b4f400ec9403c8858a
Carla Acosta <carla.acostasira@ucalgary.ca>
2024-01-29 18:30:12 -0700

    added fuctions and test_exp

---
+++ src/goph420_examples/functions.py
@@ -0,0 +1,32 @@
+def exp(x):
+    """Compute values of the exponential function"
+    for real -valued arguments.
+
+    Imputs
+    --------
+    x : float
+        The argument to the exponential function
+
+    Returns
+    --------
+    float
+
+    Raises
+    -------
+    ValueError
+        If x cannot be converted to float
+    """
+
+    x = float (x)
+    n = 0
+    fact_n = 1
+    result = 0.0
+    tol = 1.0e-16 #Tolerance
+    eps_a = 1.0
+    while eps_a > tol:
+        term = (x ** n) / fact_n
+        result += term
+        n += 1
+        fact_n *= n 
+        eps_a = abs(term / result)
+    return result
