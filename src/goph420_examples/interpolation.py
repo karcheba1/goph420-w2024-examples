f463a73766e6f6965d7c7d3ef7b10e56a8ee88b7
Carla Acosta <carla.acostasira@ucalgary.ca>
2024-01-31 21:06:46 -0700

    added interpolation.py file

---
+++ src/goph420_examples/interpolation.py
@@ -0,0 +1,27 @@
+def shape (s, order=1):
+    """Compute Lagrange interpolating polynomial 
+    shape functions of various order.
+
+    Imputs
+    -----
+    s : float
+        The local coordinate on the interval [0, 1]
+    order : int, optional, default=1
+        The order of interpolation.
+        Valid values are [1].
+
+    Returns
+    -----
+    numpy.ndarray, shape=(1, order+1)
+        The array of shape function values
+
+    Raises
+    -----
+    ValueError
+        If s cannot be converted to float.
+        If order is not in [1].
+
+    """
+    s = float(s)
+    if order not in [1]:
+        raise ValueError(f"order {order} is not valid")
