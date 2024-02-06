e30991dcbb248d83e26b5c1b34da1fba699b7be6
Carla Acosta <carla.acostasira@ucalgary.ca>
2024-02-06 09:57:02 -0700

    added interpolation_plot.py file

--- examples/interpolation_plot.py
+++ examples/interpolation_plot.py
@@ -10,7 +10,16 @@ def main():
     x = np.linspace(-5.0, 5.0, 11)
     y = np.exp(x)
 
+    #Interpolate 1 target point
+    xd = x[3:5]
+    yd = x[3:5]
+    s = 0.4
+    N = shape(s)
+    x_i = N @ xd
+    y_i = N @ yd
+
     plt.plot(x, y, 'ok', label='data')
+    plt.plot(x_i, y_i, 'xr', label='shape')
 
     # plt.show()
     plt.savefig('examples/interpolation_plot.png')
