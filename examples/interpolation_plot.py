84d4af478906341571f1a0a7bfeae133fe5b1a07
egrozell <ethan.grozell@ucalgary.ca>
2024-02-06 09:45:40 -0700

    interpolation implementation of first order larange exp(x) using 0.2 intervals for s

--- examples/interpolation_plot.py
+++ examples/interpolation_plot.py
@@ -9,8 +9,21 @@ from goph420_examples.interpolation import (
 def main():
     x = np.linspace(-5.0, 5.0, 11)
     y = np.exp(x)
+    xi = []
+    yi = []
+    for i in range (len(x)-1):
+        xd = x[i:i+2]
+        yd = y[i:i+2]
+        s = np.linspace(0,1,5)
+        for j in range (len(s)):
+            N = shape(s[j])
+            x_i = N @ xd
+            y_i = N @ yd
+            xi.append(x_i)
+            yi.append(y_i)
 
     plt.plot(x, y, 'ok', label='data')
+    plt.plot(xi, yi, '--r', label='shape')
 
     # plt.show()
     plt.savefig('examples/interpolation_plot.png')
