b7469e6a8a17908002b495ed138a7cd36d519491
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2024-02-27 10:32:02 -0700

    deleted old conflict markers

--- tests/test_exp.py
+++ tests/test_exp.py
@@ -17,7 +17,6 @@ class TestExp0(unittest.TestCase):
         self.assertIsInstance(exp(self.x), float)
 
 
-<<<<<<< HEAD
 class TestExp1(unittest.TestCase):
     def setUp(self):
         self.x = 1.0
@@ -38,7 +37,5 @@ class TestExpInvalidInput(unittest.TestCase):
             exp("one")
 
 
-=======
->>>>>>> fd42145 (implemented exp function and unit test for x=0)
 if __name__ == "__main__":
     unittest.main()
