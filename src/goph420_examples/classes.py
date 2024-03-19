a704562f5ee36ef273d6f1bea118a56915812c26
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2024-03-19 10:34:53 -0600

    ran code formatter

--- src/goph420_examples/classes.py
+++ src/goph420_examples/classes.py
@@ -234,7 +234,7 @@ class IntegrationPoint:
             If the value provided is negative
         """
         return self._temp
-    
+
     @property
     def weight(self):
         """The Gauss weights of integration.
@@ -253,10 +253,10 @@ class IntegrationPoint:
             If the value provided cannot be converted to float.
         """
         if self._weight < 0:
-            raise ValueError ("Gauss weights cannot be negative")
+            raise ValueError("Gauss weights cannot be negative")
         self._weights = float(self._weights)
         return self._weights
-    
+
     @property
     def perimeter(self):
         """The perimeter of the element.
@@ -275,10 +275,10 @@ class IntegrationPoint:
             If the value provided cannot be converted to float.
         """
         if self._perimeter < 0:
-            raise ValueError ("perimeter cannot be negative")
+            raise ValueError("perimeter cannot be negative")
         self._perimeter = float(self._perimeter)
         return self._perimeter
-    
+
     @property
     def area(self):
         """The area of the element
@@ -297,7 +297,7 @@ class IntegrationPoint:
             If the value provided cannot be converted to float.
         """
         if self._area < 0:
-            raise ValueError ("area cannot be negative")
+            raise ValueError("area cannot be negative")
         self._area = float(self._area)
         return self._area
 
@@ -319,7 +319,7 @@ class IntegrationPoint:
             If the value provided cannot be converted to float.
         """
         self._temp_inf = float(self._temp_inf)
-        return self._temp_inf 
+        return self._temp_inf
 
     @property
     def density(self):
@@ -343,10 +343,10 @@ class IntegrationPoint:
     @density.setter
     def density(self, density: float):
         if self._density < 0:
-            raise ValueError ("density cannot be negative")
+            raise ValueError("density cannot be negative")
         density = float(density)
         self._density = density
-    
+
     @property
     def thrm_cond(self):
         """The thermal conductivity of the integration point.
@@ -365,11 +365,11 @@ class IntegrationPoint:
             If the value provided cannot be converted to float.
         """
         return self._thrm_cond
-    
+
     @thrm_cond.setter
     def thrm_cond(self, thrm_cond: float):
         if self._thrm_cond < 0:
-            raise ValueError ("thermal conductivity cannot be negative")
+            raise ValueError("thermal conductivity cannot be negative")
         thrm_cond = float(thrm_cond)
         self._thrm_cond = thrm_cond
 
@@ -391,13 +391,13 @@ class IntegrationPoint:
             If the value provided cannot be converted to float.
         """
         return self._spec_heat_cap
-    
+
     @spec_heat_cap.setter
     def spec_heat_cap(self, spec_heat_cap: float):
         if self._spec_heat_cap < 0:
-            raise ValueError ("specific heat capacity cannot be negative")
+            raise ValueError("specific heat capacity cannot be negative")
         spec_heat_cap = float(spec_heat_cap)
-        self._spec_heat_cap = spec_heat_cap    
+        self._spec_heat_cap = spec_heat_cap
 
     @property
     def heat_trans_coef(self):
@@ -417,12 +417,12 @@ class IntegrationPoint:
             If the value provided cannot be converted to float.
         """
         return self._heat_trans_coef
-    
+
     @heat_trans_coef.setter
     def heat_trans_coef(self, heat_trans_coef: float):
         if self._heat_trans_coef < 0:
-            raise ValueError ("heat transfer coefficient cannot be negative")
-        heat_trans_coef = float(heat_trans_coef)        
+            raise ValueError("heat transfer coefficient cannot be negative")
+        heat_trans_coef = float(heat_trans_coef)
         self._heat_trans_coef = heat_trans_coef
 
     @temp.setter
@@ -642,7 +642,7 @@ class Element:
         P = self.int_pts[0].perimeter
         A = self.int_pts[0].area
         rho = self.int_pts[0].density
-        return h * (P/A) * self.jacobian * (1/6) * np.array([[2,1],[1,2]]) + lam * (1/self.jacobian) * np.array([[1,-1],[-1,1]])
+        return h * (P/A) * self.jacobian * (1/6) * np.array([[2, 1], [1, 2]]) + lam * (1/self.jacobian) * np.array([[1, -1], [-1, 1]])
 
     @property
     def storage_matrix(self) -> npt.NDArray[np.floating]:
